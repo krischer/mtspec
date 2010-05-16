c037d96f1a707ee3a786388d6d8ee3b2524586ec
mbyt <random.seed@web.de>
2010-05-16 09:28:55 +0000

    Moving test_dpss.py to trunk/tests

---
+++ tests/test_dpss.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import numpy as np
+import ctypes as C
+import matplotlib.pyplot as plt
+
+xx = np.random.random((512,2))
+xx = np.require(xx, 'float64', ['F_CONTIGUOUS', 'ALIGNED', 'WRITEABLE'])
+
+yy = np.random.random(2)
+yy = np.require(yy, 'float64', ['F_CONTIGUOUS', 'ALIGNED', 'WRITEABLE'])
+
+zz = np.random.random(2)
+zz = np.require(zz, 'float64', ['F_CONTIGUOUS', 'ALIGNED', 'WRITEABLE'])
+
+lib = C.CDLL('./mtspec.so')
+
+
+lib.dpss_(C.byref(C.c_int(512)), C.byref(C.c_double(2.5)), 
+          C.byref(C.c_int(2)), 
+          xx.ctypes.data_as(C.POINTER(C.c_double)), 
+          yy.ctypes.data_as(C.POINTER(C.c_double)), 
+          zz.ctypes.data_as(C.POINTER(C.c_double)))
+
+print xx
+#plt.plot(xx[:,0])
+#plt.plot(xx[:,1])
+#plt.show()
