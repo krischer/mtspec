8f6ab8b005f13c71c785397e9ac45121a724493c
mbyt <random.seed@web.de>
2010-05-17 11:29:12 +0000

    Adding init, refactoring structure.

---
+++ mtspec/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+mtspec - Multitaper Library (mtspec) Python Bindings
+====================================================
+Includes mtspec, sine_psd subroutine of the mtspec library by German A.
+Prieto.
+"""
+from multitaper import mtspec, sine_psd
