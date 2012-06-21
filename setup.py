fc298039fa770d51012d743d840e6fe0344481cb
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2012-06-22 00:06:24 +0200

    - Added proper .gitignore - Small fixes to the setup.py. Still needs quite a bit of work.

--- setup.py
+++ setup.py
@@ -7,28 +7,28 @@ mtspecPy installer.
     Moritz Beyreuther, Lion Krischer and German A. Prieto
 :license:
     GNU General Public License (GPL)
-    
+
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
     as published by the Free Software Foundation; either version 2
     of the License, or (at your option) any later version.
-    
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
-    
+
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
     02110-1301, USA.
 """
 
-from distutils.ccompiler import get_default_compiler, CCompiler
+from distutils.ccompiler import CCompiler
 from distutils.errors import DistutilsExecError, CompileError
 from setuptools import find_packages, setup
 from setuptools.extension import Extension
-import os, re
+import os
 import platform
 import sys
 
@@ -37,6 +37,9 @@ VERSION = open(os.path.join("mtspec", "VERSION.txt")).read().strip()
 macros = []
 extra_link_args = []
 extra_compile_args = []
+# XXX: Linking happens with gcc and if gfortran was installed seperately it
+# might not be able to find the gfortran library. In that case but the path to
+# libgfortan.a here ("locate libgfortran.a").
 library_dirs = []
 
 # Monkey patch CCompiler for Unix, Linux and Windows
@@ -47,10 +50,13 @@ CCompiler.language_map['.f90'] = "c"
 from distutils.unixccompiler import UnixCCompiler, _darwin_compiler_fixup
 # Monkey patch UnixCCompiler for Unix, Linux and darwin
 UnixCCompiler.src_extensions.append(".f90")
+
+
 def _compile(self, obj, src, ext, cc_args, extra_postargs, pp_opts):
         compiler_so = self.compiler_so
         if sys.platform == 'darwin':
-            compiler_so = _darwin_compiler_fixup(compiler_so, cc_args + extra_postargs)
+            compiler_so = _darwin_compiler_fixup(compiler_so, cc_args + \
+                    extra_postargs)
         if ext == ".f90":
             if sys.platform == 'darwin' or sys.platform == 'linux2':
                 compiler_so = ["gfortran"]
@@ -59,10 +65,10 @@ def _compile(self, obj, src, ext, cc_args, extra_postargs, pp_opts):
             self.spawn(compiler_so + cc_args + [src, '-o', obj] +
                        extra_postargs)
         except DistutilsExecError, msg:
-            raise CompileError, msg
+            raise CompileError(msg)
 UnixCCompiler._compile = _compile
 # set library dir for mac and linux
-libs=['gfortran']
+libs = ['gfortran']
 
 if platform.system() == "Windows":
     # Monkey patch MSVCCompiler for Windows
@@ -74,6 +80,7 @@ if platform.system() == "Windows":
     #      installed.
     from distutils.msvccompiler import MSVCCompiler
     MSVCCompiler._c_extensions.append(".f90")
+
     def compile(self, sources, output_dir=None, macros=None, include_dirs=None,
             debug=0, extra_preargs=None, extra_postargs=None,
             depends=None):
@@ -98,13 +105,13 @@ if platform.system() == "Windows":
                 self.spawn(self.compiler_so + cc_args + [src, '-o', obj] +
                            extra_postargs)
             except DistutilsExecError, msg:
-                raise CompileError, msg
+                raise CompileError(msg)
             objects.append(obj)
         return objects
 
     def link(self, target_desc, objects, output_filename, *args, **kwargs):
         os.mkdir(os.path.dirname(output_filename))
-        self.spawn(self.compiler_so + ["-shared"] + objects + 
+        self.spawn(self.compiler_so + ["-shared"] + objects +
                    ["-o", output_filename])
 
     MSVCCompiler.compile = compile
@@ -123,12 +130,12 @@ if platform.system() == "Windows":
     macros.append(('gmtime', '_gmtime64'))
 
 
-
-
-### Hack to prevent build_ext from trying to append "init" to the export symbols
+### Hack to prevent build_ext from trying to append "init" to the export
+### symbols
 class finallist(list):
     def append(self, object):
         return
+
 
 class MyExtension(Extension):
     def __init__(self, *args, **kwargs):
@@ -144,7 +151,8 @@ lib = MyExtension('mtspec',
                   extra_link_args=extra_link_args,
                   extra_compile_args=extra_compile_args,
                   #export_symbols=symbols,
-                  sources=[src + 'spectra.f90', src + 'adaptspec.f90', src + 'atanh2.f90',
+                  sources=[src + 'spectra.f90', src + 'adaptspec.f90',
+                        src + 'atanh2.f90',
                         src + 'df_spec.f90', src + 'dpss.f90',
                         src + 'dpss_ev.f90', src + 'dpss_spline.f90',
                         src + 'eigenft.f90', src + 'fdis.f90', src + 'fft.f90',
@@ -159,7 +167,7 @@ lib = MyExtension('mtspec',
                         src + 'qsnorm.f90', src + 'qtdis.f90',
                         src + 'rsm_eig.f90', src + 'set_xint.f90',
                         src + 'sft.f90', src + 'sine_cohe.f90',
-                        src + 'sine_psd.f90', 
+                        src + 'sine_psd.f90',
                         src + 'spline.f90', src + 'sym_fft.f90',
                         src + 'tinvit.f90', src + 'trbak1.f90',
                         src + 'tred1.f90', src + 'tridib.f90',
