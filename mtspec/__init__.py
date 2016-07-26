406cde7345ab0e5ec6c4495c738c32eeed383300
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-07-26 10:29:13 +0200

    Adding __version__ attribute.

--- mtspec/__init__.py
+++ mtspec/__init__.py
@@ -12,5 +12,14 @@ A Python wrapper for the multitaper library of German A. Prieto (see link_).
     GNU General Public License, Version 3
     (http://www.gnu.org/copyleft/gpl.html)
 """
+import inspect
+import os
+
 from .multitaper import mtspec, sine_psd, dpss  # NOQA
 from .multitaper import wigner_ville_spectrum, mt_coherence  # NOQA
+
+
+path = os.path.abspath(os.path.dirname(
+    inspect.getfile(inspect.currentframe())))
+with open(os.path.join(path, "VERSION.txt"), "rt") as fh:
+    __version__ = fh.read().strip()
